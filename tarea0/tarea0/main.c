515b30c72483b631ca10eb0351c7eee1bf32e12c
jachicao <jachicao@uc.cl>
2017-08-17 10:22:36 -0300

    tarea0

---
+++ tarea0/tarea0/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tarea0
+//
+//  Created by Javier on 8/17/17.
+//  Copyright © 2017 Javier. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
